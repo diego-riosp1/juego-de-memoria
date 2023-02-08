ac46c50163f801a1d3e34167007f6543e093ac46
diego-riosp1 <123577727+diego-riosp1@users.noreply.github.com>
2023-02-08 10:30:41 -0500

    second lecture

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      name: "c",
+      img: "images/c.png"
+    },
+    {
+      name: "chsarp",
+      img: "images/chsarp.png"
+    },
+    {
+      name: "cpp",
+      img: "images/cpp.png"
+    },
+    {
+      name: "html",
+      img: "images/html.png"
+    },
+    {
+      name: "css",
+      img: "images/css.png"
+    },
+    {
+      name: "fortran",
+      img: "images/fortran.png"
+    },
+    {
+      name: "c",
+      img: "images/c.png"
+    },
+    {
+      name: "chsarp",
+      img: "images/chsarp.png"
+    },
+    {
+      name: "cpp",
+      img: "images/cpp.png"
+    },
+    {
+      name: "html",
+      img: "images/html.png"
+    },
+    {
+      name: "css",
+      img: "images/css.png"
+    },
+    {
+      name: "fortran",
+      img: "images/fortran.png"
+    }
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+});
